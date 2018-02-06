b73c2d6b149a9c2eac7dda13002fe53a6d9b268a
Sergio Gonzalez <sergon2000@hotmail.com>
2018-02-07 00:27:30 +0100

    Added csv support

--- src/main/java/com/db/dynamodb/SaveItems.java
+++ src/main/java/com/db/dynamodb/SaveItems.java
@@ -5,7 +5,10 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughputExceededException;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -25,7 +28,7 @@ public class SaveItems {
 
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
                 .withRegion(Regions.EU_WEST_1)
-                .withClientConfiguration(new ClientConfiguration().withMaxErrorRetry(0))
+                .withClientConfiguration(new ClientConfiguration())
                 .build();
 
         DynamoDB dynamoDB = new DynamoDB(client);
@@ -35,13 +38,30 @@ public class SaveItems {
         String line = "";
 
         try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
-
+            int columnNumber = -1;
+            String [] headers = null;
             while ((line = br.readLine()) != null) {
-                String[] fields = line.split(splitChar);
-
+                String[] fields = line.split(splitChar,-1);
+                if (columnNumber==-1){
+                    columnNumber = fields.length;
+                    headers = fields;
+                }else{
+                    Item item = new Item();
+                    for (int i=0;i<columnNumber;i++){
+                        if (!fields[i].equals("")){
+                            item.withString(headers[i], fields[i]);
+                        }
+                    }
+                    try{
+                        table.putItem(item);
+                    }catch(ProvisionedThroughputExceededException e){
+                        System.err.println("Error inserting item: " + fields[0]);
+                    }
+                }
             }
 
-        } catch (IOException e) {
+        } catch (Exception e) {
+            System.out.println("Line: "+line);
             e.printStackTrace();
         }
 
